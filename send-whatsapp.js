7719cad02fccc9307f92f0034b50477a710f697c
sarthak mittal <48448581+mostlypanda@users.noreply.github.com>
2020-07-01 13:32:12 +0530

    Update send-whatsapp.js

--- send-whatsapp.js
+++ send-whatsapp.js
@@ -1,13 +1,13 @@
-const accountSid = 'AC12501fb836448592d2573cb762a5e940';
-const authToken = '561af201cc2d4fd01ffaa7b32c2d6464';
-const number='9358668593';
+const accountSid = 'your acount sid';
+const authToken = 'your auth token';
+const number='the number you have to send message';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
       .create({
-         from: 'whatsapp:+14155238886',
+         from: 'whatsapp:your purchased twilio no.',
          body: 'otp for the verifiction is'+parseInt(Math.random()*100000),
-         to: 'whatsapp:+919997257010'
+         to: 'whatsapp:receiptent'
        })
       .then(message => console.log(message.sid));
 
